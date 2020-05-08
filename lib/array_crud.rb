d2108063bf57ff2336250a51ad296fb98ed212c3
james Johnson <2013matthew2013@gmail.com>
2020-05-08 19:35:16 +0000

    Automatically backed up by Learn

--- lib/array_crud.rb
+++ lib/array_crud.rb
@@ -3,7 +3,7 @@ def create_an_empty_array
 end
 
 def create_an_array
-   array = ["wow", "I", "am", "really", "learning"]
+   array = ["wow", "I", "am", "arrays", "learning"]
 end
 
 def add_element_to_end_of_array(array, element)
