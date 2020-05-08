1d1eeefa87975ce249f8ffeea4f1570afa5fa3d0
james Johnson <2013matthew2013@gmail.com>
2020-05-08 19:50:50 +0000

    Automatically backed up by Learn

--- lib/array_crud.rb
+++ lib/array_crud.rb
@@ -3,16 +3,16 @@ def create_an_empty_array
 end
 
 def create_an_array
-   array =  ["I", "am", "really", "learning"]
+   array = ["I", "am", "really", "learning"]
 end
 
 def add_element_to_end_of_array(array, element)
-   array.push  ("arrays")
+   array.push ("arrays")
   
 end
 
 def add_element_to_start_of_array(array, element)
-    array.unshift("wow")
+    array.unshift ("wow")
 end
 
 def remove_element_from_end_of_array(array)
